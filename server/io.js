f4404740d8df7798e7cf96637649e0e6cc647dcc
CostaAspro708 <n10464174@qut.edu.au>
2023-05-01 02:13:23 +1000

    [SERVER][IO] added io listner to

--- server/io.js
+++ server/io.js
@@ -1,8 +1,23 @@
+require("dotenv").config()
+
+let players = [];
 
 module.exports = function(socketIO){
 
     socketIO.on('connection', (socket) => {
         console.log(`${socket.id} user just connected!`);
+    
+        socket.on('newPlayer', (data) => {
+            data.x = Math.floor(Math.random() * process.env.CANVAS_WIDTH); 
+            data.y =  Math.floor(Math.random() * process.env.CANVAS_HEIGHT);
+            data.heading = 90;
+            players.push(data);
+            console.log(players);
+            socket.emit('newPlayerResponse', players);
+        });
+    
+    
     });
+
 
 }
